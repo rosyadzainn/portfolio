c4c1a99c3bbad859dcc8319ce8e3dcef826093fb
Rosyad Zainn <zainnv@gmail.com>
2026-05-18 20:52:03 +0700

    seo: add Google Search Console verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="imvNYqUpeHpiNRoy9y9yXM9fMPfGggKvgl8iomoQGIg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <script
